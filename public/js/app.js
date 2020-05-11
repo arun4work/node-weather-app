699038e72b4849dcd50965b3227beda584deafd8
Arun Kumar Bahal <barun@TOSHIBA-TSIP.COM>
2020-05-11 08:49:03 +0530

    Remove unncessary log lines

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js file is loaded!');
-
 const weatherForm = document.querySelector('form');
 const messageOne = document.getElementById('message-1');
 const messageTwo = document.getElementById('message-2');
